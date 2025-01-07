2cbdb35836de415709679d850c430cf928bb287d
CC-Student <ovsysha16@gmail.com>
2025-01-07 16:07:24 +0100

    Add Switch class with switch)) about letters

---
+++ src/Switch.java
@@ -0,0 +1,26 @@
+public class Switch {
+    public static void main(String[] args) {
+        char ch = 'ß';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("Das ist ein Vokal.");
+                break;
+            case 'ä':
+            case 'Ä':
+            case 'Ö':
+            case 'ö':
+            case 'Ü':
+            case 'ü':
+            case 'ß':
+                System.out.println("Das ist Deutsche Buchstabe.");
+                break;
+            default:
+                System.out.println("Das ist ein Konsonant.");
+        }
+    }
+}
