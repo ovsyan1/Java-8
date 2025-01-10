5d97f7f55e4f536b7600a5991e296795a5cdea5b
Kyrylo Ovsiannik <77015424+ovsyan1@users.noreply.github.com>
2025-01-10 08:16:38 +0100

    Update BreakAndContinue.java

--- src/Basics/BreakAndContinue/BreakAndContinue.java
+++ src/Basics/BreakAndContinue/BreakAndContinue.java
@@ -13,9 +13,30 @@ public class BreakAndContinue {
         System.out.println(counter);
     }
 
+    static void b2 (){
+        int index = 0;
+		int counter = 0;
+		
+		while(counter <= Integer.MAX_VALUE) {
+			double randomValue = Math.random();
+			
+			if(randomValue >= 0.1 || randomValue <= 0.2) {
+				System.out.println(randomValue);
+				counter++;
+				if(counter == 10) {
+					break;
+				}
+			} else {
+				continue;
+			}
+			
+			index++;
+		}
+    }
+
     public static void main(String[] args) {
         b1();
         System.out.println();
-
+        b2();
     }
 }
