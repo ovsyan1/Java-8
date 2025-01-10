40e70ffb781a0fbfcce739806ae4373952c99995
Kyrylo Ovsiannik <77015424+ovsyan1@users.noreply.github.com>
2025-01-10 08:27:36 +0100

    Update Break Aufgabe

--- src/Basics/BreakAndContinue/BreakAndContinue.java
+++ src/Basics/BreakAndContinue/BreakAndContinue.java
@@ -2,15 +2,21 @@ package Basics.BreakAndContinue;
 
 public class BreakAndContinue {
     static void b1 () {
-        int counter = 0;
-        double randomValue;
-
-
-        do{
-            randomValue = Math.random();
-            counter++;
-        } while(randomValue <= 0.1 || randomValue >= 0.2);
-        System.out.println(counter);
+        int index = 0;
+	int counter = 1;
+	
+	while(index <= Integer.MAX_VALUE) {
+		double randomValue = Math.random();
+		
+		if(randomValue >= 0.1 && randomValue <= 0.2) {
+			System.out.println(randomValue);
+			break;
+		}
+		
+		counter++;
+	}
+	
+	System.out.println(counter);
     }
 
     static void b2 (){
