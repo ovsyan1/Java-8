0fb5de014cb484aa0b1fa4489a6699eec1153796
KOVS194 <ovsyasha16@gmail.com>
2025-01-17 15:07:23 +0100

    Add to te Main.java class Tahiti, homework Garbage Collection not finished yet,

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,24 @@ public class Main {
     public static void main(String[] args) {
     }
 }
+
+class Tahiti {
+    Tahiti t;
+
+    public static void main(String[] args) {
+        Tahiti t = new Tahiti();
+        Tahiti t2 = t.go(t);
+        t2 = null; // eligible for GC
+        // Zeile A
+        // ...
+    }
+
+    Tahiti go(Tahiti t) { // t.t has reference on 1 Object
+        Tahiti t1 = new Tahiti(); // t1.t has reference on 1 Object
+        Tahiti t2 = new Tahiti(); // t2.t has reference on 1 Object eligible for GC
+        t1.t = t2;
+        t2.t = t1;
+        t.t = t2;
+        return t1;
+    }
+}
