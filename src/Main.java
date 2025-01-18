b1c607da7d5f8f996a108f5707b17d118b7f47c3
Kyrylo Ovsiannik <77015424+ovsyan1@users.noreply.github.com>
2025-01-18 13:15:20 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
     }
@@ -11,17 +9,19 @@ class Tahiti {
     public static void main(String[] args) {
         Tahiti t = new Tahiti();
         Tahiti t2 = t.go(t);
-        t2 = null; // eligible for GC
+        t2 = null; // t2 eligible for GC
         // Zeile A
         // ...
     }
 
-    Tahiti go(Tahiti t) { // t.t has reference on 1 Object
-        Tahiti t1 = new Tahiti(); // t1.t has reference on 1 Object
-        Tahiti t2 = new Tahiti(); // t2.t has reference on 1 Object eligible for GC
+    Tahiti go(Tahiti t) {
+        Tahiti t1 = new Tahiti();
+        Tahiti t2 = new Tahiti(); // can't be deleted, because has reference on t
         t1.t = t2;
         t2.t = t1;
         t.t = t2;
         return t1;
     }
+
+    // Ergebnis: GC kann nur ein Objekt löschen 
 }
